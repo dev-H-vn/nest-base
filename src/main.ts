28329d7de9b28315427fa7839a53e8109372eef4
huyld <huy.ld@orai.io>
2023-07-13 14:51:15 +0700

    uuuu

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { PrismaClientExceptionFilter } from './prisma/exception.filter';
 import { AppModule } from './app.module';
+import { PrismaClientExceptionFilter } from 'src/prisma/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
